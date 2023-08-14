0b6227be6ebd65d57680784ffc0fa3f04272dd90
HazemNasser247 <105210848+HazemNasser247@users.noreply.github.com>
2023-08-15 02:15:49 +0300

    setup_favorites

---
+++ udemy-setup_favorites/lib/layout/shop_app/cubit/states.dart
@@ -0,0 +1,34 @@
+import 'package:udemy_flutter/models/shop_app/change_favorites_model.dart';
+
+abstract class ShopStates {}
+
+class ShopInitialState extends ShopStates {}
+
+class ShopChangeBottomNavState extends ShopStates {}
+
+class ShopLoadingHomeDataState extends ShopStates {}
+
+class ShopSuccessHomeDataState extends ShopStates {}
+
+class ShopErrorHomeDataState extends ShopStates {}
+
+class ShopSuccessCategoriesState extends ShopStates {}
+
+class ShopErrorCategoriesState extends ShopStates {}
+
+class ShopChangeFavoritesState extends ShopStates {}
+
+class ShopSuccessChangeFavoritesState extends ShopStates
+{
+  final ChangeFavoritesModel model;
+
+  ShopSuccessChangeFavoritesState(this.model);
+}
+
+class ShopErrorChangeFavoritesState extends ShopStates {}
+
+class ShopLoadingGetFavoritesState extends ShopStates {}
+
+class ShopSuccessGetFavoritesState extends ShopStates {}
+
+class ShopErrorGetFavoritesState extends ShopStates {}
