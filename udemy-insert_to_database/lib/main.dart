e8f973c20d6cd165ff53b940ed27b3d84bdd831d
HazemNasser247 <105210848+HazemNasser247@users.noreply.github.com>
2023-08-15 02:02:14 +0300

    insert_to_database

---
+++ udemy-insert_to_database/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_flutter/layout/home_layout.dart';
+import 'package:udemy_flutter/modules/bmi/bmi_screen.dart';
+import 'package:udemy_flutter/modules/counter/counter_screen.dart';
+import 'package:udemy_flutter/modules/login/login_screen.dart';
+import 'package:udemy_flutter/modules/messenger/messenger_screen.dart';
+import 'package:udemy_flutter/modules/users/users_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
+    );
+  }
+}
